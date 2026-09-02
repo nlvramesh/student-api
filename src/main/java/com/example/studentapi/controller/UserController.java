066856c6dc3ddc27ed6ff7200935ed762d49d410
Ramesh <leevenramesh@gmail.com>
2026-09-02 14:46:33 +0530

    Add user controller test endpoint

--- src/main/java/com/example/studentapi/controller/UserController.java
+++ src/main/java/com/example/studentapi/controller/UserController.java
@@ -19,4 +19,9 @@ public class UserController {
 
         return userService.registerUser(user);
     }
+
+    @GetMapping("/test")
+    public String test() {
+        return "User controller is working!";
+    }
 }
